74dcd1338b9ee3d3e7a2475664ff80e546861566
Robert Tabares <robtabares92@gmail.com>
2018-05-17 23:51:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(list)
+  if block_given?
+    n=0
+    output=[]
+    while n<list.length do
+      output.push(yield(list[n])) ## yield is an active block which can be pushed into another obj
+      n = n + 1
+    end
+    return output
+  else
+    return nil
+  end
+end
 
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(students) do |student|
+  student.split(" ").first
+end
 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |lang|
+  lang.upcase
+end
