da4873c96bf1baf3c57768d34b5d241ba7eca7e6
Robert Tabares <robtabares92@gmail.com>
2018-05-17 23:30:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(list)
+  if block_given?
+    n=0
+    output=[]
+    while n<list.length do
+      yield list[n]
+      output << list[n]
+      n = n + 1
+    end
+    return output
+  else
+    return nil
+  end
+end
 
-
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+#
+# languages = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(languages) do |lang|
+#   lang.upcase
+# end
