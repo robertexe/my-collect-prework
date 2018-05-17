8016c876733f5a0f1d96b37f41763a40d4417f9f
Robert Tabares <robtabares92@gmail.com>
2018-05-17 23:46:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -13,12 +13,12 @@ def my_collect(list)
   end
 end
 
-students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(students) do |student|
-  student.split(" ").first
-end
-
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(languages) do |lang|
-  lang.upcase
-end
+# students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(students) do |student|
+#   student.split(" ").first
+# end
+#
+# languages = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(languages) do |lang|
+#   lang.upcase
+# end
